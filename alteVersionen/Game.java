11a6aa63b0dbe58cc860550fbfcd284d4d9f8a15
DworshiFH <andreas.dworscak@stud.fh-campuswien.ac.at>
2021-01-02 14:59:46 +0100

    02.01.2021 Alles bis auf Split fertig implementiert

--- alteVersionen/Game.java
+++ alteVersionen/Game.java
@@ -19,7 +19,7 @@ public class Game extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Group ui = FXMLLoader.load(getClass().getResource("alteVersionen/UI.fxml"));
+        Group ui = FXMLLoader.load(getClass().getResource("alteVersionen/UI_preSave.fxml"));
 
         primaryStage.setTitle("Blackjack");
         Button btn = new Button();
--- src/main/java/at/ac/fhcampuswien/Objects/Deck.java
+++ src/main/java/at/ac/fhcampuswien/Objects/Deck.java
@@ -1,4 +1,4 @@
-package at.ac.fhcampuswien;
+package at.ac.fhcampuswien.Objects;
 
 import java.util.ArrayList;
 import java.util.Collections;
